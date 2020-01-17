311e17534c750f304c59c77c185be4f731d3b706
Soji Omiwade <omiwade@gmail.com>
2020-01-16 21:27:26 -0600

    add support for directed graphs. hardcode another test-case in

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -1,7 +1,7 @@
 import math
 from graph_priority_queue import GraphPriorityQueue
 
-def build_graph(edges):
+def build_graph(edges, *, directed = False):
     class Node:
         def __init__(self, key):
             self.key = key
@@ -19,13 +19,13 @@ def build_graph(edges):
         except:
             v = Node(iv)
         u.nbs.add(v)
-        v.nbs.add(u)
         g[iu],g[iv] = u,v
-        w[u,v] = w[v,u] = c
+        w[u,v] = c
+        if not directed:
+            v.nbs.add(u)        
+            w[v,u] = c
     return g,w
     
-def print_paths(path):
-    print(path)
 
 def initialize_single_source(g, s):
     d = {v:math.inf for v in g.values()}
@@ -47,8 +47,7 @@ def single_source_shortest_path(g, w, src):
             path[u.key] = path[pi[u].key] + [u.key] 
         for v in u.nbs:
             relax(u,v,w,d,pi,pq)            
-    return path
-    
+    return path,d
 def relax(u, v, w, d,pi,pq):
     if d[u] + w[u,v] < d[v]:
         d[v] = d[u] + w[u,v]
@@ -56,7 +55,13 @@ def relax(u, v, w, d,pi,pq):
         pq.decrease_key(v.key, d[v])
 
 if __name__ == "__main__":
-    edges = [(0,1,1), (1,2,1), (2,0,10), (2,3,1)]
-    g, w = build_graph(edges)
-    path = single_source_shortest_path(g, w, 3)
-    print_paths(path)
+    # edges = [(0,1,1), (1,2,1), (2,0,10), (2,3,1)]
+    # src = 3
+    s,t,x,y,z = 4,3,1,0,2 #"stxyz"
+    edges = [(s,t,10),(s,y,5), (t,x,1), (t,y,2), (y,t,3), (y,z,2), (y,x,9), (x,z,4), (z,s,7), (z,x,6)]
+    src = s
+    g, w = build_graph(edges, directed = True)
+    path, d = single_source_shortest_path(g, w, src)
+    print(path)
+    for v,vdist in d.items():
+        print(v.key, vdist)
