20383614596a50a5cce44af49fbe8273378154e7
Soji Omiwade <omiwade@gmail.com>
2020-01-17 11:18:54 -0600

    besides formatting and unit-testing, in line ~42 added ability for code to work correctly even if there's no path to some nodes.

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -1,4 +1,4 @@
-import math
+from math import inf
 from graph_priority_queue import GraphPriorityQueue
 
 def build_graph(edges, *, directed = False):
@@ -25,14 +25,13 @@ def build_graph(edges, *, directed = False):
             v.nbs.add(u)        
             w[v,u] = c
     return g,w
-    
 
 def initialize_single_source(g, s):
-    d = {v:math.inf for v in g.values()}
+    d = {v:inf for v in g.values()}
     d[s] = 0
     pi = {v:None for v in g.values()}
     return d, pi
-    
+
 def single_source_shortest_path(g, w, src):
     s = g[src]
     d, pi = initialize_single_source(g, s)
@@ -43,11 +42,14 @@ def single_source_shortest_path(g, w, src):
     while not pq.is_empty():
         u = g[pq.extract_min()]
         S.add(u)
-        if u is not s:
+        if d[u] == inf:
+            path[u.key] = []
+        elif u is not s:
             path[u.key] = path[pi[u].key] + [u.key] 
         for v in u.nbs:
             relax(u,v,w,d,pi,pq)            
     return path,d
+
 def relax(u, v, w, d,pi,pq):
     if d[u] + w[u,v] < d[v]:
         d[v] = d[u] + w[u,v]
@@ -55,12 +57,12 @@ def relax(u, v, w, d,pi,pq):
         pq.decrease_key(v.key, d[v])
 
 if __name__ == "__main__":
-    # edges = [(0,1,1), (1,2,1), (2,0,10), (2,3,1)]
-    # src = 3
-    s,t,x,y,z = 4,3,1,0,2 #"stxyz"
-    edges = [(s,t,10),(s,y,5), (t,x,1), (t,y,2), (y,t,3), (y,z,2), (y,x,9), (x,z,4), (z,s,7), (z,x,6)]
-    src = s
-    g, w = build_graph(edges, directed = True)
+    edges = [(0,1,1), (1,2,1), (2,0,10), (2,3,1)]
+    src = 3
+    # s,t,x,y,z = 4,3,1,0,2 #"stxyz"
+    # edges = [(s,t,10),(s,y,5), (t,x,1), (t,y,2), (y,t,3), (y,z,2), (y,x,9), (x,z,4), (z,s,7), (z,x,6)]
+    # src = s
+    g, w = build_graph(edges, directed = False)
     path, d = single_source_shortest_path(g, w, src)
     print(path)
     for v,vdist in d.items():
