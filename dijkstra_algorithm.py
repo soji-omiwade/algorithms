8bfee82cb35b2c74075825378b18bc9c5cd661c5
Soji Omiwade <omiwade@gmail.com>
2020-01-16 18:51:38 -0600

    dijsktra shortest path code works with simple case. will try a more involved case later.

--- dijkstra_algorithm.py
+++ dijkstra_algorithm.py
@@ -1,11 +1,14 @@
 import math
+from graph_priority_queue import GraphPriorityQueue
+
 def build_graph(edges):
     class Node:
         def __init__(self, key):
             self.key = key
             self.nbs = set([])
-    g={}
-    w={}
+        def __str__(self):
+            return str(self.key)
+    g,w={},{}
     for iu,iv,c in edges:
         try:
             u = g[iu]
@@ -15,46 +18,45 @@ def build_graph(edges):
             v = g[iv]
         except:
             v = Node(iv)
+        u.nbs.add(v)
+        v.nbs.add(u)
         g[iu],g[iv] = u,v
-        w[u,v] = c
+        w[u,v] = w[v,u] = c
     return g,w
     
 def print_paths(path):
-    for np in paths.values():
-        print(np)
+    print(path)
 
 def initialize_single_source(g, s):
     d = {v:math.inf for v in g.values()}
     d[s] = 0
     pi = {v:None for v in g.values()}
-    return d,pi
+    return d, pi
     
 def single_source_shortest_path(g, w, src):
     s = g[src]
-    d,pi = initialize_single_source(g, s)
-    pq = PriorityQueue([, src)
+    d, pi = initialize_single_source(g, s)
+    pq = GraphPriorityQueue(len(g), src)
     S = set([])
-    path = {v:None for v in g.values()}
-    path[s] = str(s.key)
-    while pq:
+    path = {}
+    path[src] = [src]
+    while not pq.is_empty():
         u = g[pq.extract_min()]
+        S.add(u)
         if u is not s:
-            path[u] = path[pi[u]] + "-" + str(u.key)
-        if u is not t:
-            S.add(u)
-            for v in u.nbs:
-                relax(u,v,w,d,pi,pq)
-                
+            path[u.key] = path[pi[u].key] + [u.key] 
+        for v in u.nbs:
+            relax(u,v,w,d,pi,pq)            
     return path
     
 def relax(u, v, w, d,pi,pq):
     if d[u] + w[u,v] < d[v]:
         d[v] = d[u] + w[u,v]
         pi[v] = u
-        pq.decrease_key(v.key, d[v])   
+        pq.decrease_key(v.key, d[v])
 
 if __name__ == "__main__":
-    edges = [(0,1,1), (1,2,1), (2,0,1), (2,3,1) ]
-    g,w = build_graph(edges)
+    edges = [(0,1,1), (1,2,1), (2,0,10), (2,3,1)]
+    g, w = build_graph(edges)
     path = single_source_shortest_path(g, w, 3)
     print_paths(path)
--- graph_priority_queue.py
+++ graph_priority_queue.py
@@ -1,54 +1,61 @@
+import math
 class GraphPriorityQueue:
-    def __init__(self, g:dict, src:int):
-        a = [None]
-        for i in range(1,1+len(g)):
-            a.append((math.inf, g[i-1]))
-        a[src+1] = 0
-        self.heap = self.MinHeap(a)
-        
+    def __init__(self, n, src):
+        self.heap = self.MinHeap(n, src)
+    
     def is_empty(self):
         return self.heap.size == 0
         
-    def decrease_key(self, i, key):
-        i += 1
+    def decrease_key(self, v_key, key):
+        i = self.heap.b[v_key]
         if key > self.heap.a[i]:
             raise ValueError("new key is greater than current key")
         self.heap.a[i] = key
-        self.heap.heapify(i)
+        self.heap.reverse_heapify(i)
         
     def extract_min(self):
         if self.heap.size < 1: 
             raise Exception(f"heap underflow at size {self.heap.size}")
-        min_key=self.heap.a[1]
-        self.heap.a[1]=self.heap.a[self.heap.size]
+        min_key = self.heap.b[0]
+        self.heap.a[0] = self.heap.a[self.heap.size-1]
+        self.heap.b[0] = self.heap.b[self.heap.size-1]
         self.heap.size -= 1
-        self.heap.heapify(1)
-        return min_key[1]
+        self.heap.heapify(0)
+        return min_key
     
     class MinHeap:
-        def __init__(self, a):
-            self.a = a
-            self.size = len(a)-1
-            for i in range(math.floor(self.size/2), 0, -1):
+        def __init__(self, n, src):
+            self.a = [math.inf for _ in range(n)]
+            self.a[src] = 0
+            self.b = [i for i in range(n)]
+            self.size = n
+            for i in range(math.floor(self.size/2)-1, -1, -1):
                 self.heapify(i)
                 
+        def parent(self, i):
+            return math.floor((i-1)/2)
+            
         def left(self, i):
-            return 2*i
+            return 2*i + 1
             
         def right(self, i):
-            return 2*i+1
-            
-        def pi(self, i):
-            return math.floor(i/2)
-            
+            return 2*(i+1)
+                 
+        def reverse_heapify(self, i):
+            while i > 0 and self.a[self.parent(i)] > self.a[i]:
+                self.a[i], self.a[self.parent(i)] = self.a[self.parent(i)], self.a[i]
+                self.b[i], self.b[self.parent(i)] = self.b[self.parent(i)], self.b[i] 
+                i = self.parent(i)
+       
         def heapify(self, i): 
             l = self.left(i)
             r = self.right(i)
             smallest = i
-            if l <= self.size and self.a[l] < self.a[i]:
+            if l < self.size and self.a[l] < self.a[i]:
                 smallest = l
-            if r <= self.size and self.a[r] < self.a[smallest]:
+            if r < self.size and self.a[r]< self.a[smallest]:
                 smallest = r
             if smallest != i:
-                self.a[i],self.a[smallest] = self.a[smallest],self.a[i]
+                self.a[i], self.a[smallest]= self.a[smallest], self.a[i]
+                self.b[i], self.b[smallest] = self.b[smallest], self.b[i]
                 self.heapify(smallest)
