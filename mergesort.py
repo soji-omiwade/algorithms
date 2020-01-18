62fb35c37667e688c3ec7d9ac834f816f5c6c53c
Soji Omiwade <omiwade@gmail.com>
2020-01-18 13:08:53 -0600

    this new version removes the use of +/- 1 everywhere. more intuitive perhaps to a programmer. prior one is mor intuitive to a mathematician

--- mergesort.py
+++ mergesort.py
@@ -1,19 +1,19 @@
 def mergesort(a,p,r):
-    if p < r:
+    if p + 1 < r:
         q = (p+r)//2
         mergesort(a,p,q)
-        mergesort(a,q+1,r)
+        mergesort(a,q,r)
         merge(a,p,r)
     
 def merge(a,p,r,b = []):
     if b == []:
         b=[0 for _ in a]
-    q = (p+r)//2
-    for i in range(p,r+1):
+    for i in range(p,r):
         b[i] = a[i]
-    lp, rp, count = p, q+1, 0
-    while count < r-p+1:
-        if rp > r or (lp <= q and b[lp] < b[rp]):
+    q = (p+r)//2
+    lp, rp, count = p, q, 0
+    while count < r-p:
+        if rp == r or (lp < q and b[lp] < b[rp]):
             a[count+p] = b[lp]
             lp += 1
         else:
@@ -29,5 +29,5 @@ if __name__ == "__main__":
     print(a)
     sorted_a = sorted(a)
     assert a != sorted_a
-    mergesort(a,0,n-1)
+    mergesort(a,0,n)
     assert sorted_a == a
