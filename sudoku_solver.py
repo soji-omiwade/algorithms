73fffcd7999f20ce00ed0e851e065e71d595b885
Soji Omiwade <omiwade@gmail.com>
2020-01-19 21:56:38 -0600

    this modified solver will report a fail, if there are invalid values in the input itself! will test in leetcode tomorrow!

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -12,41 +12,63 @@ def find_empty_cell(grid)->(int,int):
                 return i,j
     return n,n
 
-def is_safe(grid, row, col, val)->bool:
-    """check whether it will be safe to assign val to specified location"""
-    not_safe = in_row(grid, row, val) or in_col(grid, col, val) \
-            or in_box(grid, row, col, val)
-    return not not_safe
+def is_valid_rc(grid, row, col)->bool:
+    """check for validity of the grid with respect to row and col
+    
+    this means the row, column and box, corresponding to the parameters
+    "row" and "col" do not violate sudoku rules.
+    """
+    is_valid = is_valid_row(grid, row) and is_valid_col(grid, col) \
+            and is_valid_box(grid, row, col)
+    return is_valid
         
-def in_row(grid, row, val):
+def is_valid_row(grid, row):
+    s = set([])
+    zero_count = 0
     for c in range(n):
-        if grid[row][c] == val:
-            return True
+        if grid[row][c] == 0:
+            zero_count += 1
+        else:
+            s.add(grid[row][c])
+    if len(s) == n - zero_count:
+        return True
     return False
 
-def in_col(grid, col, val):
+def is_valid_col(grid, col):
+    s = set([])
+    zero_count = 0
     for r in range(n):
-        if grid[r][col] == val:
-            return True
+        if grid[r][col] == 0:
+            zero_count += 1
+        else:
+            s.add(grid[r][col])
+    if len(s) == n - zero_count:
+        return True
     return False
-    
-def in_box(grid, row, col, val):
+
+def is_valid_box(grid, row, col):
     R, C = row//3, col//3
+    zero_count = 0
+    s = set([])
     for i in range(3):
         for j in range(3):
-            if grid[3*R+i][3*C+j] == val:
-                return True
+            if grid[3*R+i][3*C+j] == 0:
+                zero_count += 1
+            else:
+                s.add(grid[3*R+i][3*C+j])
+                
+    if len(s) == n - zero_count:
+        return True
     return False
-    
+
 def solve_sudoku(grid)->bool:
     r,c = find_empty_cell(grid)
     if (r,c) != (n,n):
         for val in range(1,1+n):
-            if is_safe(grid, r, c, val):
-                grid[r][c] = val
-                if solve_sudoku(grid):
-                    return True
-                grid[r][c] = 0
+            grid[r][c] = val
+            if is_valid_rc(grid, r, c) and solve_sudoku(grid):
+                return True
+            grid[r][c] = 0
     else:
         return True
     #there is an empty cell, but I can't safely use it!
@@ -59,7 +81,7 @@ def print_grid(grid):
         
 if __name__ == "__main__":
     grid = \
-        [[3, 0, 6, 5, 0, 8, 4, 0, 0], 
+        [[5, 0, 6, 5, 0, 8, 4, 0, 0], 
         [5, 2, 0, 0, 0, 0, 0, 0, 0], 
         [0, 8, 7, 0, 0, 0, 0, 3, 1], 
         [0, 0, 3, 0, 1, 0, 0, 8, 0], 
