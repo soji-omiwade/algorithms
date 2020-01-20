d1a62c8639b53c3573edf0a710307c5cbfeaa78c
Soji Omiwade <omiwade@gmail.com>
2020-01-19 20:45:14 -0600

    learning how to use pdb extensively

---
+++ amala.py
@@ -0,0 +1,31 @@
+def okele(foo):
+    # breakpoint()
+    foo += 1
+    foo = spam
+    # breakpoint()
+    print(foo)
+    return foo + 42
+    
+def ogbono(measles):
+    a = dict(one="42", two=-1)
+    breakpoint()
+    try: 
+        okele(2*measles)
+    except: 
+        print("gotcha!")
+    breakpoint()
+    return "all good"
+    
+def fop():
+    a = 42
+    a += 1
+    # breakpoint()
+    print(a)
+    
+if __name__ == "__main__":
+    rice = "43"
+    dodo = 20
+    fish = 42
+    val = ogbono(fish)
+    print(val)
+    
