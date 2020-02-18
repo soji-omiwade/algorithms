3099859b4c0a2502aa032d6f7b111c2378dfbd3d
Soji Omiwade <omiwade@gmail.com>
2020-02-18 04:34:10 -0600

    almost there.

--- find_maximum_function_value_fast_without_function_equation.cs
+++ find_maximum_function_value_fast_without_function_equation.cs
@@ -1,48 +1,48 @@
 /**complexity
 time:
 **/
-public static Tuple<double,int> binfind(a,p,r,bestnpv)
+public static Tuple<double,int> binfind(a,p,r,max_func_val)
 {
 	if (p==r)
 	{
-		return (bestnpv,p);
+		return (max_func_val,p);
 	}
 	
 	q=(int)(p+r)/2;
-	npvq = npv(q)
-	if (npvq > bestnpv)
+	if (func(q) > max_func_val)
 	{
-		return binfind(a,q+1,r,npvq)
+		return binfind(a,q+1,r,func(q))
 	}
-	else
-	{
-		return binfind(a,p,q-1,bestnpv)
-	}
+	return binfind(a,p,q-1,max_func_val)
 }
 
 /**
-parent_npv is the best npv so far. otherwise we wouldn't be going to 
+parent_func_value is the best func so far. otherwise we wouldn't be going to 
 look for better
 time: O(sigma i=1 up to lg(maximum) f(i)) where f is the time it takes
 		to compute the i-th frac stage
 space: O(1)
 **/
-public static Tuple<int,int> hopper(a, start, maximum, parent_npv)
+public static Tuple<int,int> hopper(start, maximum, parent_func_value)
 {
-	if (start>maximum)
+	if (start>=maximum)
 	{
-		return Tuple((int)start/2+1,maximum)
+		//like the if below, start/2+1 would be wrong because
+		//we don't yet know that it is not better than the maximum
+		return Tuple((int)start/2,maximum)
 	}
 	
-	if (npv(start)<parent_npv)
+	if (func(start)<=parent_func_value)
 	{
-		return Tuple((int)start/2+1, start)
+		//start/2+1 would be wrong since start/2 could be the optimal!
+		//Similarly, maximum=start is wrong because we know for a fact
+		//that start cannot be the optimal (it
+		//func(start/2))
+		return Tuple((int)start/2, start-1)  
 	}
-	if (npv(start)>parent_npv)
-	{
-		return hopper(a,2*i,maximum,npv(start))
-	}
+	
+	return hopper(2*start,maximum,func(start))
 }
 
-start,end=hopper(a,1,60,bestnpv=0)
-bestnpv, opt_frac = binfind(a,start,end,0)
+start,end=hopper(a,1,60,max_func_val=0)
+max_func_val, opt_frac = binfind(a,start,end,0)
