9c5c91ca7b71b6c20eda3a105ca7495b261f367e
Soji Omiwade <omiwade@gmail.com>
2020-02-21 02:35:27 -0600

    add longest substring function, based on the input charset

--- longest_substring_no_repeating_characters_feb_2020.py
+++ longest_substring_no_repeating_characters_feb_2020.py
@@ -1,14 +1,25 @@
+def longest_charset_based(s):
+    next_loc=[0]*128
+    im,jm=1,0; i=0
+    for j in range(len(s)):
+        i=max(i,next_loc[ord(s[j])])
+        next_loc[ord(s[j])]=j+1
+        if j-i>jm-im:
+            im,jm=i,j
+    return im,jm,s[im:jm+1]
+
+
 def longest_dict_based(s):
     d={}
     i=0
     im,jm=1,0
     for j in range(len(s)):
         if s[j] in d:
-            i=max(i,s[j])
+            i=max(i,d[s[j]])
         d[s[j]]=j+1
         if j-i>jm-im:
             im,jm=i,j
-    return im,jm,s[im:jm]
+    return im,jm,s[im:jm+1]
 """
 s="abcdcbkkek"
 res="bcda"
@@ -33,9 +44,14 @@ def longest_substring_no_repeating_chars_set(s):
     return im,jm,s[im:jm+1]
 
 s="abcdabkkek"
-res="bcda"    
 print(longest_substring_no_repeating_chars_set(s))
+assert  (longest_substring_no_repeating_chars_set(s) 
+            == longest_dict_based(s)
+            == longest_charset_based(s))
 
 import sys
 s=sys.argv[1]
 print(longest_substring_no_repeating_chars_set(s))
+assert  (longest_substring_no_repeating_chars_set(s) 
+            == longest_dict_based(s)
+            == longest_charset_based(s))
