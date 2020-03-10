7ad2e5bcb0ed05c45069206a91ef66f65ba9db7d
Soji Omiwade <omiwade@gmail.com>
2020-03-10 15:25:53 -0500

    now works for any mxn matrix, not just nxn

--- print_reverse_diagonals.py
+++ print_reverse_diagonals.py
@@ -1,16 +1,31 @@
 def print_reverse_diagonals(a):
-    n=len(a)
+    res=[]
+    m=len(a)
+    n=len(a[0])
     for j in range(n-1,-1,-1):
-        for i in range(n-j):
-            print(a[i][j+i])
-    for i in range(1,n):
-        for j in range(n-i):
-            print(a[j+i][j])
-            
+        for i in range(min(m,n-j)):
+            res.append(a[i][j+i])
+    for i in range(1,m):
+        for j in range(min(m-i,n)):
+            res.append(a[j+i][j])
+    return res        
 grid=[
 "1234",
 "abcd",
 "5678",
-"efgh"
+"efgh",
 ]
-print_reverse_diagonals(grid)
+assert print_reverse_diagonals(grid) == list("43d2c81b7ha6g5fe")
+grid=[
+"1234",
+"5678",
+]
+assert print_reverse_diagonals(grid) == list("43827165")
+grid=[
+"12",
+"34",
+"56",
+"78",
+]
+# print(print_reverse_diagonals(grid))
+assert print_reverse_diagonals(grid) == list("21436587")
