74893960fd068b7f4495eff1b119a1a4005ac0fa
Soji Omiwade <omiwade@gmail.com>
2020-07-11 15:43:11 -0700

    as titled

---
+++ super/diamond_class_insider_but_behind.py
@@ -0,0 +1,54 @@
+class D:
+    cake = "D"
+    @classmethod
+    def foo(cls, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert D.cake == "D"
+        assert len(args) == 0
+        assert not hasattr(super(D, cls), "foo")
+        return D.cake
+        
+class C(D):
+    cake = "C"
+    @classmethod
+    def foo(cls, c, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert super().cake == "D"
+        assert len(args) == 0
+        assert c == "cc"
+        assert hasattr(super(), "foo")
+        return C.cake + super().foo(**kwargs)
+        
+class B(D):
+    cake = "B"
+    @classmethod
+    def foo(cls, b, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert super().cake == "C"
+        assert len(args) == 0
+        assert b == "bb"
+        assert hasattr(super(), "foo")
+        return B.cake + super(B, cls).foo(**kwargs)
+
+class A(B, C):
+    cake = "A"
+    @classmethod
+    def foo(cls, a, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert super().cake == "B"
+        assert len(args) == 0
+        assert a == "aa"
+        assert hasattr(super(), "foo")
+        return A.cake + super().foo(**kwargs)
+
+class E(A): 
+    @classmethod
+    def foo(cls, *args, **kwargs):
+        raise Exception("shouldn't be called")
+
+obj = A()
+assert A.mro() == [A, B, C, D, object]
