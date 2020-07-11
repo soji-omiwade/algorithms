f092f6f3f95cdeb239405c469190c725c0309446
Soji Omiwade <omiwade@gmail.com>
2020-07-11 15:37:14 -0700

    how super works when a class is there, but it isnt on the linearization. and when skipping too.

---
+++ super/diamond_class_outsider.py
@@ -0,0 +1,60 @@
+class E: 
+    @classmethod
+    def foo(cls, *args, **kwargs):
+        raise Exception("shouldn't be called")
+        
+class D:
+    cake = "D"
+    @classmethod
+    def foo(cls, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert D.cake == "D"
+        assert len(args) == 0
+        assert not hasattr(super(D, cls), "foo")
+        return D.cake
+        
+class C(D):
+    cake = "C"
+    @classmethod
+    def foo(cls, c, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert super().cake == "D"
+        assert len(args) == 0
+        assert c == "cc"
+        assert hasattr(super(), "foo")
+        return C.cake + super().foo(**kwargs)
+        
+class B(D):
+    cake = "B"
+    @classmethod
+    def foo(cls, b, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert super().cake == "C"
+        assert len(args) == 0
+        assert b == "bb"
+        assert hasattr(super(), "foo")
+        return B.cake + super(B, cls).foo(**kwargs)
+
+class A(B, C):
+    cake = "A"
+    @classmethod
+    def foo(cls, a, *args, **kwargs):
+        assert cls is A
+        assert cls.cake == "A"
+        assert super().cake == "B"
+        assert len(args) == 0
+        assert a == "aa"
+        assert hasattr(super(), "foo")
+        return A.cake + super(E, cls).foo(**kwargs)
+
+obj = A()
+try: 
+    A.foo(a="aa", b="bb", c="cc") == "ACD"
+except TypeError as e: 
+    pass
+else:
+    raise Exception("super(E, cls) should have faile")
+assert A.mro() == [A, B, C, D, object]
