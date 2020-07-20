a70553e1adb750030e94e5eb718208c13ea60044
Soji Omiwade <omiwade@gmail.com>
2020-07-20 11:26:32 -0700

    done on paper. no need to type. reviewed what I did

---
+++ two_sum.2020_07_20_via_sorting.py
@@ -0,0 +1,12 @@
+from typing import List
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """done on paper. no need to type. reviewed what I did 
+        
+        a couple weeks ago (same problem)
+        """
+        pass
+                
+assert Solution().twoSum([2,7,11,15], 9) == [0, 1]
+assert Solution().twoSum([21,2,7,11,15, 21], 42) == [0, 5]
+assert Solution().twoSum([2,7,11,4,15,-3], 8) == [2, 5]
