41a167f533345ab121f5b949500804ac5239b007
Soji Omiwade <omiwade@gmail.com>
2020-07-30 11:00:48 -0700

    my own implementation. simple

---
+++ best_time_to_buy_and_sell_a_stock_2020_07_30.py
@@ -0,0 +1,14 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxprofit = 0
+        minbuy = float("inf")
+        for price in prices:
+            if price < minbuy:
+                minbuy = price
+            maxprofit = max(maxprofit, price - minbuy)
+        return maxprofit
+        
+maxprofit = Solution().maxProfit
+assert maxprofit([7, 3, 2, 3, 9, 0, 6, 3]) == 7
+assert maxprofit([7, 3, 2, 3, 9, 0, 6, 9, 3]) == 9
