9d83eb77d4b9841a8757e3b9500d9d9342329cfb
Soji Omiwade <osoji@hotmail.com>
2021-05-18 11:05:03 -0500

    ok. pretty cool way of ending the binary search. cool meaning it terminates when the range you want is found! me likee

--- shifted_array_search.py
+++ shifted_array_search.py
@@ -1,47 +1,43 @@
 from typing import List
-def shifted_array_search(arr: List[int], key: int):
-    '''
-    find start index sidx
-        . divide array: lo, mid hi
-        
-        . if arr[lo] < arr[mid] < arr[hi] or lo = mid = hi: return lo
-        . if arr[mid] > arr[hi]
-            lo = mid + 1 
-        . if arr[lo] > arr[mid]: it is here (and could be mid)
-            hi = mid
-    then use sidx and sidx + (n - 1) in a regular binary search
-    '''
-    n = len(arr)
-    lo = 0
-    hi = n - 1
-    while True:
-        mid = (lo + hi) // 2
-        if (arr[lo] < arr[mid] < arr[hi] or lo == mid == hi or (lo == mid and arr[lo] < arr[hi])):
-            sidx = lo
-            break
-        if arr[mid] > arr[hi]:
-            lo = mid + 1
-        elif arr[lo] > arr[mid]:
-            hi = mid
-        else:
-            raise Exception("cant be here")
-    lo = sidx
-    hi = (sidx - 1) % n
-    while True:
-        if hi < lo: 
+class Solution:
+    def search(self, arr: List[int], key: int) -> int:
+        n = len(arr)
+        lo = 0
+        hi = n - 1
+        while arr[hi] < arr[lo]:
+            mid = (lo + hi) // 2
+            if arr[lo] > arr[mid]: # or lo == mid:
+                hi = mid
+            elif arr[mid] > arr[hi]:
+                lo = mid + 1
+            else:
+                raise Exception("should not be here")
+        hi = lo - 1
+        if lo > hi:
             hi += n
-        mid = (lo + hi) // 2 % n
-        if arr[mid] == key:
-            return mid
-        if arr[mid] > key:
-            hi = mid - 1
-        else:
-            lo = mid + 1
-    raise Exception("not found")
-    
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if arr[mid % n] == key:
+                return mid % n
+            if arr[mid % n] > key:
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return -1
+# 0    1   2  3  4         
 #[10, 11, 12, 3, 4, 5, 6, 7, 8, 9] [10, 11, 12, 3, 4, 5, 6, 7, 8, 9]
+shifted_array_search = Solution().search
 arr = [i for i in range(10)]
 arr = [arr[i] + 10 for i in range(3)] + arr[3:]
 assert (shifted_array_search(arr, 11) == 1)
 assert (shifted_array_search(arr, 9) == 9)
 assert (shifted_array_search(arr, 5) == 5)
+arr = [4,5,6,7,0,1,2]
+key = 3
+assert (shifted_array_search(arr, key) == -1)
+arr = [4,5,6,7,0,1,2]
+key = 4
+assert (shifted_array_search(arr, key) == 0)
+arr = [4,5,6,7,0,1,2]
+key = 7
+assert (shifted_array_search(arr, key) == 3)
