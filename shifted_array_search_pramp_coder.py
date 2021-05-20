fe13d47ad21d8f0bb0fb18073c2287e46c0e8916
Soji Omiwade <omiwade@gmail.com>
2021-05-20 18:42:14 -0500

    fixed the 'is the left side sorted issue'

--- shifted_array_search_pramp_coder.py
+++ shifted_array_search_pramp_coder.py
@@ -1,14 +1,10 @@
 def shifted_arr_search(shiftArr, num):
-  '''
-  [9, 12, 17, 2, 4, 5]  num = 12
-  [9, 12, 17, 12, 4, 5]  num = 4
-  '''
   start, end = 0, len(shiftArr)-1
   while start<=end:
     mid = start+(end-start)//2
     if shiftArr[mid] == num:
       return mid
-    if shiftArr[mid] > shiftArr[start]: # the left part of the arr is sorted
+    if shiftArr[mid] >= shiftArr[start]: # the left part of the arr is sorted
         if num >= shiftArr[start] and num< shiftArr[mid]: # arr[start] <= num < arr[mid]
           end = mid-1
         else:
@@ -20,5 +16,9 @@ def shifted_arr_search(shiftArr, num):
           end = mid-1 
   return -1
   
-print(shifted_arr_search([3,2], 3))
-print(shifted_arr_search([3,2], 2))
+assert shifted_arr_search([3,2], 3) == 0
+assert shifted_arr_search([3,2], 2) == 1
+assert shifted_arr_search([9, 12, 17, 2, 4, 5], 12) == 1
+assert shifted_arr_search([9, 12, 17, 2, 4, 5], 2) == 3
+assert shifted_arr_search([9, 12, 17, 12, 4, 5], 4) == 4
+assert shifted_arr_search([9, 12, 17, 12, 4, 5], 42) == -1
