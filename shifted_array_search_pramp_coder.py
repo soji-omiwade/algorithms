e3172328b7b84f33a38f1acee78975de7ae51dd5
Soji Omiwade <omiwade@gmail.com>
2021-05-20 18:33:14 -0500

    evidence that though it passed all pramp cases, this shifted array search is broken when you have [x, y], y such that x > y

---
+++ shifted_array_search_pramp_coder.py
@@ -0,0 +1,24 @@
+def shifted_arr_search(shiftArr, num):
+  '''
+  [9, 12, 17, 2, 4, 5]  num = 12
+  [9, 12, 17, 12, 4, 5]  num = 4
+  '''
+  start, end = 0, len(shiftArr)-1
+  while start<=end:
+    mid = start+(end-start)//2
+    if shiftArr[mid] == num:
+      return mid
+    if shiftArr[mid] > shiftArr[start]: # the left part of the arr is sorted
+        if num >= shiftArr[start] and num< shiftArr[mid]: # arr[start] <= num < arr[mid]
+          end = mid-1
+        else:
+          start = mid+1
+    else:
+        if num> shiftArr[mid] and num<= shiftArr[end]: # arr[mid] < num <= arr[end]
+          start = mid+1
+        else:
+          end = mid-1 
+  return -1
+  
+print(shifted_arr_search([3,2], 3))
+print(shifted_arr_search([3,2], 2))
