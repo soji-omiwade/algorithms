c6da405e05e5e45f6044edc71e138f345bfa7692
Soji Omiwade <omiwade@gmail.com>
2021-05-24 17:03:38 -0700

    read the question rwong. BFS can actually be used!

--- same_tree.py
+++ same_tree.py
@@ -11,21 +11,23 @@ class Solution:
         if bool(p) ^ bool(q):
             return False
         plevel = deque(p)
-        pcount = qcount = 1
         qlevel = deque(q)
-        while plevel or qlevel:
-            paux = []
-            quax = []
+        while plevel and qlevel:
             for _ in range(len(plevel)):
                 pnode = plevel.popleft()
                 qnode = qlevel.popleft()
-                paux.append(node)
-                
-                plevel.append(node.left)
-                plevel.append(node.right)
-                
-            if paux != qaux or reverse
-                return False
+                if bool(pnode.left) ^ bool(qnode.left):
+                    return False
+                if pnode.left:
+                    if pnode.left.val != qnode.left.val:
+                        return False
+                    plevel.append(pnode.left)
+                    qlevel.append(qnode.left)
+                if bool(pnode.right) ^ bool(qnode.right):
+                    return False
+                if pnode.right:
+                if pnode.right and pnode.right.val != qnode.right.val:
+                    return False
         if bool(plevel) ^ bool(qlevel):
             return False
         return True
