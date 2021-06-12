cc4b1bcf716262b65ba2e31fa2a9910d6de56f01
Soji Omiwade <omiwade@gmail.com>
2021-06-11 22:17:09 -0500

    kinda hard to say which is best. but bst looks to be worst! maybe because of call stack of going through tree. or could it be because of generator use to go through tree???

--- past-questions/linkedin_2021_06_07_intervals_stress_test.py
+++ past-questions/linkedin_2021_06_07_intervals_stress_test.py
@@ -1,6 +1,8 @@
 from linkedin_2021_06_07_intervals import Intervals
 from random import randrange
 from pprint import pprint
+import timeit
+import sys
 
 '''
 width = 5
@@ -9,30 +11,42 @@ for a count times
     start = rand between 0 and count - 1
     end = rand between start + 1 and count
 '''
-width = 5
-count = 1000
+def test_compare_all_interval_types(intervals_types):
+    prevres = None
+    for interval in intervals_types:
+        for (start, end) in input_intervals:
+            interval.addInterval(start, end)
+            res = interval.getTotalCoveredLength()
+            if prevres:
+                assert prevres == res
 
+def time_compare_add_intervals(interval_type):
+    res = []
+    for (start, end) in input_intervals:
+        interval_type.addInterval(start, end)
 
-intervals_types = (
-    Intervals.init_as_bst(),
-    Intervals.init_as_dll(),
-    Intervals.init_as_list(),
-    Intervals.init_as_set()
-    )
+def time_compare_get_covered_length():
+    interval_type.getTotalCoveredLength()
 
-import sys
 if len(sys.argv) != 2:
     raise Exception(f"should be 2 args, not {len(sys.argv)}")
+count = 1000
 input_intervals = []
 for _ in range(count):
     start = randrange(0, count - 2)
     end = randrange(start + 1, start + int(sys.argv[1]))
     input_intervals.append((start, end))
-pprint(input_intervals)
-prevres = None
-for interval in intervals_types:
-    for (start, end) in input_intervals:
-        interval.addInterval(start, end)
-        res = interval.getTotalCoveredLength()
-        if prevres:
-            assert prevres == res
+
+intervals_types = (Intervals.init_as_bst(), Intervals.init_as_dll(),
+                    Intervals.init_as_list(), Intervals.init_as_set())
+reskeys = ["bst", "dll", "list", "set"]
+res_vals = []
+number = 100
+for interval_type in intervals_types:
+    addtime = timeit.timeit('time_compare_add_intervals(interval_type)',number=number, globals=globals())
+    lentime = timeit.timeit('time_compare_add_intervals(interval_type)',number=number, globals=globals())
+    res_vals.append((addtime, lentime))
+
+pprint(dict(zip(reskeys, res_vals)))
+# test_compare_all_interval_types()
+
