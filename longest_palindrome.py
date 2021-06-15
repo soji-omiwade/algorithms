aeb845adf374e28ae42ec2e3eec3d722cb851e14
Soji Omiwade <omiwade@gmail.com>
2021-06-15 13:56:08 -0500

    looked through the input too quickly. so took a while to understand the problem. made this mistake before.

---
+++ longest_palindrome.py
@@ -0,0 +1,31 @@
+'''
+input/output
+
+
+dccaccd -> number of pairs of same character you can take out
+if somethings (that are not pairs) left then
+    add 1
+
+build a counter from the string
+go through and add to an aggregrate the val count // 2
+
+----
+
+
+
+
+
+
+'''
+from collections import Counter
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        counter = Counter(s)
+        res = 0
+        count_one_exists = 0
+        for ch, count in counter.items():
+            res += 2 * (count // 2)
+            if count % 2 == 1:
+                count_one_exists = 1
+        return res + count_one_exists
+        
