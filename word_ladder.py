4c0f5005ac4c323439e560ebd21d0efd6ee81e1c
Soji Omiwade <omiwade@gmail.com>
2021-07-12 09:39:31 -0500

    so the implementation is correct. but the hacks i am making wont reduce the overall complexity. gonne need to use the alphabet non-preprocessing way. see comment inside.

--- word_ladder.py
+++ word_ladder.py
@@ -6,14 +6,24 @@ class Solution:
             for let, otherlet in zip(word, otherword):
                 if let != otherlet:
                     step += 1
+                    if step > 1:
+                        return False
             return step == 1
         
         unprocessed_words = deque([beginWord])
         distance = defaultdict(int)
+        wordList_set = set(wordList)
+        wordList_set.add(beginWord)
+        #complexity =                                               n^2 * m = 25m * 10 =  250m
+        #could be made =                                            n*ksi + n^2 = ???   this one is big-omega of n * ksi
+        #another way is quite interesting: no preprocessing         n*ksi +            this one doesn't have the big-omega component!
         while unprocessed_words:
             word = unprocessed_words.popleft()
-            for otherword in wordList:
+            wordList_set.remove(word)
+            for otherword in wordList_set:
                 if distance[otherword] == 0 and onestep(word, otherword):
+                    if otherword == endWord:
+                        return distance[word] + 2
                     distance[otherword] = 1 + distance[word]
                     unprocessed_words.append(otherword)
         if distance[endWord] != 0:
