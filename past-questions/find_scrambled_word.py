7c4f20b04fe07881f66b05ceae111d998b1a7bcb
Soji Omiwade <omiwade@gmail.com>
2021-08-09 13:44:00 -0500

    is subsequence

--- past-questions/find_scrambled_word.py
+++ past-questions/find_scrambled_word.py
@@ -1,20 +1,57 @@
+from typing import List, Optional
+from collections import Counter
+
+'''
+string = bbabylkkj
+sorted = 
+0123456789
+abbbjkkly
+alist = [0]
+blist = [1,2,3]
+jlist = [4]
+klist = [5,6]
+llist = [7]
+ylist = [8] 
+now given word, say baby.
+for each letter in word
+    loc = binsearch in lookup[letter]
+    if loc is outside of that lookup[letter], then it isn't in T
+example
+
+cat -> act
+baby -> abby
+
+0123456789
+abbbjkkly
+act
+abby
+
+bsear
+
+   
+- sort every single string!
+- string to arrays (of indexes). example: 
+    string
+- 
+'''            
+# def binsearch_find_scrambled(words: List[str], string:str) -> Optional[str]:
+    # last = 0
+    # for letter in word:
+        # for idx in range(last, len(string)):
+            # if string[idx] == word[idx]:
+                # break
+            # for sletter in 
+    # else:
+        # return word
+
 '''
 T: W*L
 S: LW but could be L + W
+
+fis
+f***i*****s*****
 '''
-words = ["cat", "baby", "dog", "bird", "car", "ax"]
-string1 = "tcabnihjs"
-string2 = "tbcanihjs"
-string3 = "baykkjl"
-string4 = "bbabylkkj"
-string5 = "ccc"
-string6 = "breadmaking"
-
-from typing import List
-from collections import Counter
-
-
-def find_scrambled(words: List[str], string:str):
+def hashmap_find_scrambled(words: List[str], string:str) -> Optional[str]:
     stringcounter = Counter(string) #L
     wordcounter = {}
     for word in words:
@@ -27,8 +64,63 @@ def find_scrambled(words: List[str], string:str):
         else:
             return word
     return None
-words = ["cat", "baby", "dog", "bird", "car", "ax"
-        
-        ]
-string1 = "tcabnihjs"
-print(find_scrambled(words, string6))
+
+'''
+fis
+f***i*****s*****
+'''
+def subseq_find_scrambled(words: List[str], string:str) -> Optional[str]:
+    string = sorted(string) #tcabnihjs -> abchijnst
+    for word in words: #cat -> act
+        listword = sorted(word)
+        widx = stringidx = 0
+        while widx < len(listword) and stringidx < len(string):
+            if listword[widx] == string[stringidx]:
+                widx, stringidx = widx + 1, stringidx + 1
+            else:
+                stringidx += 1
+        if widx == len(listword):
+            return word
+    return None
+
+'''
+fis
+f***i*****s*****
+'''
+def forsubseq_find_scrambled(words: List[str], string:str) -> Optional[str]:
+    liststring = sorted(string) #tcabnihjs -> abchijnst
+    for word in words: #cat -> act
+        listword = sorted(word)
+        nextidx = 0
+        for letter in listword:
+            for idx, stringletter in enumerate(liststring[nextidx:], nextidx):
+                if stringletter == letter:
+                    nextidx = idx + 1
+                    break
+            else: #listword is not a subsequence
+                break
+        else:
+            return word
+    return None
+    
+words = ["cat", "baby", "dog", "bird", "car", "ax"]
+strings = ["tcabnihjs"
+    , "tbcanihjs"
+    , "baykkjl"
+    , "bbabylkkj"
+    , "ccc"
+    , "breadmaking"
+    , "xavier"
+    , ""
+    ]
+results = ["cat", "cat", None, "baby", None, "bird", "ax", None]
+find_scrambled_methods = (hashmap_find_scrambled
+    , subseq_find_scrambled
+    , forsubseq_find_scrambled
+    )
+for string, result in zip(strings, results):
+    for find_scrambled in find_scrambled_methods:
+        findresult = find_scrambled(words, string)
+        print(find_scrambled.__name__, findresult, result, string)
+        assert(findresult == result)
+    print()
