fa903b56ee38303bf7c09c77641d3269f14d15c9
Soji Omiwade <omiwade@gmail.com>
2021-08-11 12:41:21 -0500

    the helper can do the reset of pred and succ; seems more natural to me

--- kth_smallest_element_in_a_sorted_matrix_independent_of_k.py
+++ kth_smallest_element_in_a_sorted_matrix_independent_of_k.py
@@ -39,9 +39,9 @@ mid = 6 => count = 7
 
 '''
 
-def kth_smallest(matrix: List[List[int]], k: int) -> int:
+def kthSmallest(matrix: List[List[int]], k: int) -> int:
     def countlessorequal(mid):
-        nonlocal predormid, succ
+        predormid, succ = matrix[0][0], matrix[n-1][n-1]
         count = 0
         row, col = n - 1, 0
         while row >= 0 and col < n:
@@ -52,8 +52,8 @@ def kth_smallest(matrix: List[List[int]], k: int) -> int:
             else:
                 succ = min(succ, matrix[row][col])
                 row -= 1
-        return count
-        
+        return count, predormid, succ
+
     n = len(matrix)
     if k > n * n or n == 0:
         raise ValueError
@@ -61,12 +61,11 @@ def kth_smallest(matrix: List[List[int]], k: int) -> int:
         return matrix[0][0]
     start = matrix[0][0]
     end = matrix[n-1][n-1]
-    
+
     # import time
     while start < end: # not needed. we will find a k
         mid = (start + end) // 2
-        predormid, succ = matrix[0][0], matrix[n-1][n-1]
-        count = countlessorequal(mid)
+        count, predormid, succ = countlessorequal(mid)
         #count 3 elements less than mid
         # print(f"start = {start}, end = {end}, mid = {mid}, count = {count}", end='\t')
         # print(f"pre = {predormid}, succ = {succ}")
@@ -79,9 +78,9 @@ def kth_smallest(matrix: List[List[int]], k: int) -> int:
         # time.sleep(.5)
     return start
      #1 2 5 7
-        # p s
-      # st    end
-# m = 4
+            # p s
+          # st    end
+    # m = 4
       
 matrix = [            
     [1, 3, 5]
@@ -89,7 +88,7 @@ matrix = [
     , [3, 5, 11]
 ]
 k = 5
-print(kth_smallest(matrix, k))
+print(kthSmallest(matrix, k))
 
 '''
 1 2 3 4 5
@@ -107,9 +106,9 @@ matrix = [[(i+1)*(j+1) for j in range(5)] for i in range(5)]
 pprint(matrix)
 
 k = 5
-print(kth_smallest(matrix, k)) # 3
+print(kthSmallest(matrix, k)) # 3
 
 matrix = [[(i+1)*(j+1) for j in range(5)] for i in range(5)]
 k = 9
-print(kth_smallest(matrix, k)) # 5
+print(kthSmallest(matrix, k)) # 5
 
