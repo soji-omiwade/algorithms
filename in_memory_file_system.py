da4fb3e32d14067e0cc570e9de497ba806dd1370
Soji Omiwade <omiwade@gmail.com>
2021-08-13 11:52:19 -0500

    typo missing 'be'

--- in_memory_file_system.py
+++ in_memory_file_system.py
@@ -8,7 +8,7 @@ have fun
 Problem
 Design an in-memory file system to simulate the following functions:
 
-ls: Given a path in string format. If it is a file path, return a list that only contains this file’s name. If it is a directory path, return the list of file and directory names in this directory. Your output (file and directory names together) should in lexicographic order.
+ls: Given a path in string format. If it is a file path, return a list that only contains this file’s name. If it is a directory path, return the list of file and directory names in this directory. Your output (file and directory names together) should be in lexicographic order.
 
 mkdir: Given a directory path that does not exist, you should make a new directory according to the path. If the middle directories in the path don’t exist either, you should create them as well. This function has void return type.
 
