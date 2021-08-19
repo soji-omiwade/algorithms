b98a8f96751bfec8ee1b56c1beb7d26dfe12f2bf
Soji Omiwade <omiwade@gmail.com>
2021-08-19 12:58:54 -0700

    finished coding second part

--- past-questions/twitter_2021_08_spaces.py
+++ past-questions/twitter_2021_08_spaces.py
@@ -39,5 +39,55 @@ now you are given logs, but we want to know the top k by number of people
 inside such
 
 solution: use heap..why top k
-but need to account for 
+but need to account for  changes to heap priorities! let's do it!!
+
+heapentry: [usercount, spacename]
+heapentrylookup: spacename --> heapentry
+
 '''
+import heapq
+def setup():
+    heapentrylookup = defaultdict(lambda: [0, None])
+    spacesheap = []
+    return heapentrylookup, spacesheap
+    
+'''
+    ["create", "abc", "1", "100"], 
+'''
+def action_happened(action, spacename, userid, timestamp, spacesheap, heapentrylookup):    
+    #request should be appended to requests esp. for timestamp
+    
+    heapentry = heapentrylookup[spacename]
+    heapentry[-1] = "REMOVED"   
+    newheapentry = heapentry[:]
+    heapentrylookup[spacename] = newheapentry
+    
+    if action == "leave":    
+        newheapentry[0] += 1
+    else: #create or join
+        newheapentry[0] -= 1
+    heapq.push(spacesheap, newheapentry)
+
+def topk(spacesheap, k):
+    '''
+    remove k; then put them back before returning 
+    OR
+    copy the list; remove k; and return: O(n) ---> no no
+    '''
+    have = 0
+    tempstore = []
+    res = []
+    while have < k:
+        heapentry = heapq.heappop(spacesheap)
+        if heapentry[-1] != "REMOVED":
+            tempstore.append(heapentry)
+            res.append(heapentry[-1], - heapentry[0])
+            have += 1
+    for heapentry in tempstore:
+        heapq.heappush(heappentry)
+    return res
+    
+setup()
+for action in requests:
+    action_happened(*action)
+print(topk)
