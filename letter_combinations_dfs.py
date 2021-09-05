f0dfcd4d11411f7e0cd6ef2ffc236edc16e7a4ba
Soji Omiwade <omiwade@gmail.com>
2021-09-04 22:38:00 -0700

    letter tile combos while printing all paths

--- letter_combinations_dfs.py
+++ letter_combinations_dfs.py
@@ -20,15 +20,16 @@ function helper(path)
 '''
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        def helper():
+        def helper(path):
             res = 0
             for letter in count:
                 if count[letter] > 0:
                     count[letter] -= 1
-                    res += 1 + helper()
+                    res += 1 + helper(path + [letter])
+                    print("".join(path + [letter]))
                     count[letter] += 1
             return res
 
         count = Counter(tiles)
-        return helper() # the answer
+        return helper([]) # the answer
 
