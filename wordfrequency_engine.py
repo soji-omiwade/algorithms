9ea57ef659c9f46e6b19520622892b2bd20cfb2d
Soji Omiwade <omiwade@gmail.com>
2021-09-23 21:29:40 -0500

    I got it correct. Awesomegit add . git add .

--- wordfrequency_engine.py
+++ wordfrequency_engine.py
@@ -14,14 +14,44 @@ approach:
             finalres.append([word, str(freq)])
     return finalres
 '''
+from collections import Counter
 def wordorder(sen: str) -> List[Tuple[str, str]]:
-    ...
+    if not sen:
+        return []
+    # clean out each word from non alphanum characters
+    sen = [   "".join(letter for letter in word if letter.isalnum()) for word in sen.split(" ")]
+    # initialize a counter (note that the order from left to right is actually preserved)
+        # Ill 1, be 3, starting 1, ...
+    counter = Counter(sen)
+    # create a array res, with length len(sen), with empty lists, 
+    # for each word, freq in counter
+        # res[freq].append(word)
+    res = [[] for _ in range(1 + len(sen))]
+    for word, freq in counter.items():
+        res[freq].append(word)
+    # return res
+    # finalres = []
+    # for freq, lis in enumerate(res):
+        # for word in lis:
+            # finalres.append([word, str(freq)])
+    # return finalres
+    
+    finalres = []
+    # print(res)
+    for freq, lis in enumerate(reversed(res)):
+        tfreq = len(res) - 1 - freq
+        for word in lis:
+            finalres.append([word, str(tfreq)])
+    return finalres
     
 sen = "I'll be starting at an exciting startup, guys!!"
 print(wordorder(sen)) #[("Ill", "1"), ("be", "1"), ...]
 
-sen = "I'll be be be starting at an an exciting startup, guys!!"
+sen = "I'll be be b^e star7ting at an an exciting startup, guys!!"
 print(wordorder(sen)) #[("be", "3"), ("an", "2"), ("Ill", "1"), ("starting", "1"), ...]
 
 sen = ""
 print(wordorder(sen)) #[]
+
+sen = "I'll"
+print(wordorder(sen)) #[("Ill", 1)]
