87e55ffbcfaae332d3dbefc3a8b76c65344ee250
Soji Omiwade <omiwade@gmail.com>
2021-09-28 14:34:31 -0700

    add tracker to see how much work each thread does: sometimes equal amount; other times far not. also took out the print statement for len content

--- realpython/concurrency.py
+++ realpython/concurrency.py
@@ -3,19 +3,24 @@ import time
 import threading
 from concurrent.futures import ThreadPoolExecutor
 import sys
+from collections import Counter
 
 thread_local = threading.local()
 threading_count = int(sys.argv[1])
+labordivision = Counter()
 
 def downloadsite(url, session):
     with session.get(url) as response:
-        print(f"Read {len(response.content)} from url")
+        #print(f"Read {len(response.content)} from url")
+        pass
         
 def downloadsites_threading(urls):
     def downloadsite(url):
         session = get_session()
         with session.get(url) as response:
-            print(f"Read {len(response.content)} from url")
+            #print(f"Read {len(response.content)} from url")
+            pass
+        labordivision[threading.get_ident()] += 1
 
     def get_session():
         if not hasattr(thread_local, "session"):
@@ -48,7 +53,7 @@ if __name__ == "__main__":
     starttime = time.time()
     downloadsites_threading(sites)
     res.append(time.time() - starttime)
+    print(labordivision)
     
     for approach, runtime in zip(approaches, res):
-        print(approach, "\t", runtime )
-    # print(f"downloaded {len(sites)} in {duration} seconds")
+        print(approach, "\t", f"{runtime:.1f}" )
