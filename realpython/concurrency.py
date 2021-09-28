2a75a9162accc6c107f58fe3119d39c89dd3cf48
Soji Omiwade <omiwade@gmail.com>
2021-09-28 13:50:26 -0700

    having one thread per download actually had better performance than 5 threads; but the difference seems marginal--less than a sec

--- realpython/concurrency.py
+++ realpython/concurrency.py
@@ -1,11 +1,32 @@
 import requests
 import time
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import sys
+
+thread_local = threading.local()
+threading_count = int(sys.argv[1])
 
 def downloadsite(url, session):
     with session.get(url) as response:
         print(f"Read {len(response.content)} from url")
         
-def downloadsites(urls):
+def downloadsites_threading(urls):
+    def downloadsite(url):
+        session = get_session()
+        with session.get(url) as response:
+            print(f"Read {len(response.content)} from url")
+
+    def get_session():
+        if not hasattr(thread_local, "session"):
+            thread_local.session = requests.Session()
+        return thread_local.session
+
+    with ThreadPoolExecutor(max_workers=threading_count) as executor:
+        executor.map(downloadsite, urls)
+        
+
+def downloadsites_synch(urls):
     with requests.Session() as session:
         for url in urls:
             downloadsite(url, session)
@@ -14,10 +35,20 @@ if __name__ == "__main__":
     sites = [
         "https://www.jython.org",
         "http://olympus.realpython.org/dice",
-        "https://www.cnn.com",
-        "https://www.youtube.com",
     ] * 80
+    approaches = ["synchronous", "threading"]
+    res = []
+    
+    #synchronous
     starttime = time.time()
-    downloadsites(sites)
-    duration = time.time() - starttime
-    print(f"downloaded {len(sites)} in {duration} seconds")
+    downloadsites_synch(sites)
+    res.append(time.time() - starttime)
+
+    #threading
+    starttime = time.time()
+    downloadsites_threading(sites)
+    res.append(time.time() - starttime)
+    
+    for approach, runtime in zip(approaches, res):
+        print(approach, "\t", runtime )
+    # print(f"downloaded {len(sites)} in {duration} seconds")
