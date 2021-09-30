16c5acaafdaddb18e1ad0b9cb3ca610678843a0e
Soji Omiwade <omiwade@gmail.com>
2021-09-30 06:55:07 -0700

    not working

---
+++ wordladder_pramp.py
@@ -0,0 +1,33 @@
+'''
+bfs
+consider one letter change at a time and advance in BFS only if changed is in words
+keep track of bfs path length
+'''
+from collections import deque
+def shortestWordEditPath(source, target, words):
+  
+  '''
+  fish ... aish ... bish ...
+  '''
+  def getnext(word):
+    newword = list(word)
+    for i in range(len(word)):
+      ignorech = word[i]
+      for j in range(26):
+        ch = chr(j + ord('a'))
+        if ch != ignorech:
+          newword[i] = ch
+          if ''.join(newword) in wordsset:
+            yield newword
+      newword[i] = ignorech
+
+  wordsset = set(words)
+  q = deque([source])
+  count = 0
+  while q:
+    for i in range(len(q)):
+      word = q.popleft()
+      for nextword in getnext(word):
+        q.append(nextword)
+    count += 1
+  return count
