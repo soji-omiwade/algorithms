cb16bd269aa590a717bba90eecac3813f456c50a
Soji Omiwade <omiwade@gmail.com>
2021-10-06 09:04:26 -0500

    works now when multiple items have the same position

--- vertical_order_traversal.py
+++ vertical_order_traversal.py
@@ -18,17 +18,15 @@ class Solution:
                 minval, maxval = min(minval, col), max(maxval, col)
                 
                 #process node here...
-                lookup[col].append((node.val, row)) #look
-                lis = lookup[col]
-                previtem = None if len(lis) == 1 else lis[-2]
-                if False and previtem and previtem[1] == row and previtem[0] > node.val:
-                    lis[-1], lis[-2] = lis[-2], lis[-1]
+                lookup[col].append((row, node.val)) #look
                 
                 if node.left:
                     q.append((node.left, row + 1, col - 1))
                 if node.right:
                     q.append((node.right, row + 1, col + 1))
                     
+        for lis in lookup.values():
+            lis.sort()
         for i in range(minval, maxval + 1):
-            res.append([(val, row) for val, row in lookup[i]])
+            res.append([val for (row, val) in lookup[i]])
         return res
