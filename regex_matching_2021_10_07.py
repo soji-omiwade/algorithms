00d2cb811326a2d64ee255cb04c833dd695c223d
Soji Omiwade <omiwade@gmail.com>
2021-10-07 22:46:57 -0500

    perfecto!

--- regex_matching_2021_10_07.py
+++ regex_matching_2021_10_07.py
@@ -25,21 +25,58 @@ oooog, ooig
 
 o*g ooog
 o*g oog
-def matches(pattern, string)
-    if not pattern and string:
-        return False
+
+o*g..., oog...  fail
+o*g..., og...   fail
+o*g..., g...
+def matches(pattern, string)   o*g..., oog...
+    if not pattern:
+        return not string
         
     if len(pattern) > 1 and pattern[1] == "*"
         return (matches(pattern[2:], string)
                     or (string[0] == pattern[0] and matches(pattern, string[1:]))
                )
     return (
-            string
+            bool(string)
             and pattern[0] in (string[0], '.') 
             and matches(pattern[1:], string[1:])
 ...
 '''
 
 def matches(pattern: str, string: str) -> bool:
-    return (pattern[0] == '.' or pattern[0] == string[0] 
-        and matches(pattern[1:], string[1:])
+    if not pattern:
+        return not string
+        
+    if len(pattern) > 1 and pattern[1] == "*":
+        return (matches(pattern[2:], string)
+                    or (string[0] == pattern[0] and matches(pattern, string[1:]))
+               )
+    return (
+            bool(string)
+            and pattern[0] in (string[0], '.') 
+            and matches(pattern[1:], string[1:])
+           )
+           
+'''
+    fish        fisk     -> false
+    fi.h        fish     -> true
+    do*g        dog      -> true
+    f.*y        friday   -> true
+    da*bq*q     dbqq     -> true
+
+'''           
+pattern, string = "fish", "fisk"           
+print(matches(pattern, string)) # F
+
+pattern, string = "fi.h", "fish"           
+print(matches(pattern, string)) # T
+
+pattern, string = "do*g", "dog"           
+print(matches(pattern, string)) # T
+
+pattern, string = "o*g...", "oog.K."           
+print(matches(pattern, string)) # T
+
+pattern, string = "o*g.K.", "oog..."           
+print(matches(pattern, string)) # F
