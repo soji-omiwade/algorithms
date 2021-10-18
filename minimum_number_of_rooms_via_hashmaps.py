6f86bec77dff5e52a95e44db00d51ec561c1165e
Soji Omiwade <omiwade@gmail.com>
2021-10-18 13:45:41 -0500

    works; will test for the bigger example

--- minimum_number_of_rooms_via_hashmaps.py
+++ minimum_number_of_rooms_via_hashmaps.py
@@ -12,11 +12,54 @@ class Solution:
             
         ans = 0
         cnt = 0
-        times = sorted(d)
-        for t in times:
-            # print(t, d[t])
-            cnt += d[t]
+        freqlookup = sorted(d.items())
+        for inttime, intfreq in freqlookup:
+            cnt += intfreq
             ans = max([ans, cnt])
         
         return ans
 
+sol = Solution()
+a = [ [0, 30]       #---------------------
+    , [5, 10]       #   -----
+    , [15,20] ]     #          ------
+print(sol.solve(a)) # 2
+
+
+a = [ [1, 18]   #------------------
+    , [18,23]   #                  ----
+    , [15,29]   #              ----------
+    , [4, 15]   #    ----------
+    , [2, 11]   # ----------
+    , [5, 13]   #     -------
+    ]
+
+'''
+freqlookup
+
+
+
+freqlookup:
+0: 1
+5: 1
+15: 1
+
+30: -1
+10: -1
+20: -1
+
+freqlookup: [ 0,1  5,1  10,-1  15,1  20,-1  30,-1]
+cnt: 0 1 2 1 2 1 0
+    
+    
+
+'''
+
+a = [ [1, 18]   #------------------
+    , [18,23]   #                  ----
+    , [15,29]   #              ----------
+    , [4, 15]   #    ----------
+    , [2, 11]   # ----------
+    , [5, 13]   #     -------
+    ]
+print(sol.solve(a)) # 4
