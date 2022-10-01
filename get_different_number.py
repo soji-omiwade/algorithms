8ece1013825ffbbf9bc03ff9eda8fb68321a4c14
Soji Omiwade <soji.omiwade@rubrik.com>
2022-10-01 07:10:58 -0700

    careful of using enumerate if arr elem changes, and you use it in a while in the for

---
+++ get_different_number.py
@@ -0,0 +1,32 @@
+'''
+for 0 1 [1, 0, 1000, 205, 30]
+0 1 [1, 0, 1000, 205, 30]
+0 1 [0, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+0 1 [1, 1, 1000, 205, 30]
+'''
+def get_different_number(arr):
+  count = 0
+  for idx, idxelem in enumerate(arr):
+    while idx != idxelem and (idxelem < len(arr)) and count < 10:
+      count += 1
+      temp = arr[idxelem] # idxelem = 42, idx = 0, arr[idxelem] = 58
+      arr[idxelem] = idxelem # 
+      idxelem = arr[idx] = temp
+  for idx, idxelem in enumerate(arr):
+    if idx != idxelem:
+      return idx
+  return len(arr)
+
+#arr = [0, 1, 2, 3]
+#print(get_different_number(arr)) #4
+
+arr = [1, 0, 1000, 205, 30]
+print(get_different_number(arr)) #2
+
